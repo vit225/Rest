45cd29829bb2692513566b80463352d24d02ed95
Виталий <vitaly@mail.ru>
2022-04-29 19:29:16 +1000

    изменил fetch = FetchType.EAGER на fetch = FetchType.LAZY ошибок не заметил

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -21,7 +21,7 @@ public class User implements UserDetails {
     private String lastName;
     @Column(name = "password")
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
